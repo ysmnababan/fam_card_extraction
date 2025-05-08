411bafd537497ef8e3a4e53d7b5024a981dba650
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-05-08 22:28:21 +0700

    feat: add preprocess for json

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import ImageProcessor as ip
-
+import FamilyData as fd
 # === Config ===
 TARGET_IMAGE_PATH = './img/kk1.jpeg'
 TEMPLATE_IMAGE_PATH = './templ/kk_template.png'
 OUTPUT_ALIGNED_PATH = './output/aligned_target.png'
 CROP_OUTPUT_DIR = 'cropped_cells'
-
+JSON_OUTPUT_PATH = "./output/kk_data.json"
+PROCESSED_JSON_PATH = "./output/processed_data.json"
 if __name__ == '__main__':
     processor = ip.ImageProcessor(
         TARGET_IMAGE_PATH,
@@ -14,6 +15,10 @@ if __name__ == '__main__':
         CROP_OUTPUT_DIR
     )
     # processor.run()
-    processor.extract_table()
-    processor.extract_header()
-    processor.extract_footer()
+    # processor.extract_table()
+    # processor.extract_header()
+    # processor.extract_footer()
+
+    family = fd.FamilyData.from_json_file(JSON_OUTPUT_PATH)
+    family.preprocess(PROCESSED_JSON_PATH)
+    family.print_all()
