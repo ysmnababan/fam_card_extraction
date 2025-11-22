801a425ce50e80faeaa1312e44821e057dad2a05
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-11-22 14:42:23 +0700

    add 'error' and 'info'

--- logger.py
+++ logger.py
@@ -1,8 +1,14 @@
 # logger.py
 LOG_TEMPLATE_WITH_STEP = "\n============================================\n\033[92m[Pipeline]\033[0m (%d/%d) %s"
+LOG_TEMPLATE_SIMPLE = "%s[Pipeline]%s %s"
+LOG_TEMPLATE_INFO = " %s[Info]%s %s"
+LOG_TEMPLATE_ERROR = " %s[ERROR]%s %s"
 
-# def log(msg: str):
-#     print(LOG_TEMPLATE % msg)
+COLOR_GREEN = "\033[92m"
+COLOR_YELLOW = "\033[93m"
+COLOR_RED = "\033[91m"
+COLOR_RESET = "\033[0m"
+
 _current_step = 0
 _total_steps = 0
 
@@ -18,3 +24,12 @@ def log(msg: str):
     global _current_step
     _current_step += 1
     print(LOG_TEMPLATE_WITH_STEP % (_current_step, _total_steps, msg))
+
+def info(msg: str):
+    """Info log (yellow)"""
+    print(LOG_TEMPLATE_INFO % (COLOR_YELLOW, COLOR_RESET, msg))
+
+
+def error(msg: str):
+    """Error log (red)"""
+    print(LOG_TEMPLATE_ERROR % (COLOR_RED, COLOR_RESET, msg))
